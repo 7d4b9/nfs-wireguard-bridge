81592d02da1107b7ee25c81ec58407c09750d738
Vegito Admin <admin@vegito.app>
2025-12-08 07:21:13 +0000

    feature(pdc): remove custom internal wg client connect for basic setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,18 +18,12 @@ trap cleanup EXIT
 export WG_IF="wg0"
 export WG_SUBNET="10.8.0"
 export WG_SERVER_IP="${WG_SUBNET}.1"
-export WG_CLIENT_IP="${WG_SUBNET}.2"
 
 nfs-start.sh &
 bg_pids+=($!)
 
-wg-connect.sh &
-bg_pids+=($!)
-
-# exec wg-start.sh
 
 wg-start.sh &
 bg_pids+=($!)
 
-# wg-quick up "${WG_IF}"
 wait -n
